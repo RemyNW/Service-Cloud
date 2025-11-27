91206552a0ee3d9c431e5c9df7d70ed249659138
RemyNW <remynowe33@live.fr>
2025-11-27 12:36:07 +0100

    update main.tf

--- main.tf
+++ main.tf
@@ -22,8 +22,8 @@ provider "github" {
 resource "github_repository" "repo_terraform_demo" {
   name        = "repo-cree-par-terraform"
   description = "Repo GitHub créé automatiquement avec Terraform depuis le TP Service-Cloud"
-  visibility  = "private"   # ou "public" selon l'énoncé
+  visibility  = "public"
 
   has_issues = true
-  auto_init  = true   # crée un README automatiquement
+  auto_init  = true   
 }
